d900afc70ac650cde30d253e80bbfd1ac3e8c5ac
chenyouwei3 <2148275751@qq.com>
2024-07-06 15:02:10 +0800

    gg

--- app/router/authCenter.go
+++ app/router/authCenter.go
@@ -8,23 +8,23 @@ import (
 func AuthCenterRouter(engine *gin.Engine) {
 	user := engine.Group("/user")
 	{
-		user.POST("/create", controller.CreateUser)     //增user*
-		user.DELETE("/deleted", controller.DeletedUser) //删除user*
-		user.PUT("/update", controller.UpdatedUser)     //修改*
-		user.GET("/get", controller.GetUser)            //查找*
+		user.POST("/create", controller.UserController{}.CreateUser)     //增user*
+		user.DELETE("/deleted", controller.UserController{}.DeletedUser) //删除user*
+		user.PUT("/update", controller.UserController{}.UpdatedUser)     //修改*
+		user.GET("/get", controller.UserController{}.GetUser)            //查找*
 	}
 	role := engine.Group("/role")
 	{
-		role.POST("/create", controller.CreateRole)     //增role*
-		role.DELETE("/deleted", controller.DeletedRole) //删role*
-		role.PUT("/update", controller.UpdatedRole)     //改role******************
-		role.GET("/get", controller.GetRole)            //查role*
+		role.POST("/create", controller.RoleController{}.CreateRole)     //增role*
+		role.DELETE("/deleted", controller.RoleController{}.DeletedRole) //删role*
+		role.PUT("/update", controller.RoleController{}.UpdatedRole)     //改role******************
+		role.GET("/get", controller.RoleController{}.GetRole)            //查role*
 	}
 	api := engine.Group("/api")
 	{
-		api.POST("/create", controller.CreateApi)     //增api*
-		api.DELETE("/deleted", controller.DeletedApi) //删api*
-		api.PUT("/update", controller.UpdatedApi)     //改api*
-		api.GET("/get", controller.GetApi)            //查api*
+		api.POST("/create", controller.ApiController{}.CreateApi)     //增api*
+		api.DELETE("/deleted", controller.ApiController{}.DeletedApi) //删api*
+		api.PUT("/update", controller.ApiController{}.UpdatedApi)     //改api*
+		api.GET("/get", controller.ApiController{}.GetApi)            //查api*
 	}
 }
